4dd5fe062d1807b12dff995e1327cc7cbec328a3
mmarquezs <mmsa1994@gmail.com>
2017-03-08 11:40:45 +0100

    * Added comments to ScreenManager * Simplified fadeIn and fadeOut with JavaFX Transitions.

--- src/main/java/com/doominoo/ScreensManager.java
+++ src/main/java/com/doominoo/ScreensManager.java
@@ -1,5 +1,6 @@
 package com.doominoo;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
@@ -7,6 +8,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.util.Duration;
 
 import java.util.HashMap;
@@ -15,31 +18,61 @@ import java.util.HashMap;
  * Created by marcmarquez on 06/02/17.
  */
 public class ScreensManager extends StackPane {
+    /**
+     * Manages the screens of the program, based on :
+     * https://blogs.oracle.com/acaicedo/entry/managing_multiple_screens_in_javafx1
+     */
+    private Stage window;
     // Screen list.
-    private static HashMap<String, Node> screens = new HashMap<>();
+    private static HashMap<String, AppScreen> screens = new HashMap<>();
 
-    void addScreen(String name, Node screen) {
-        // I don't like this too much.
-        ((AppScreen) screen).setScreensManager(this);
+    void setWindow(Stage window) {
+        /**
+         * Sets the stage to be used as "window".
+         */
+        this.window = window;
+    }
+
+    Stage getWindow() {
+        /**
+         * Gets the "window" used by the screen manager.
+         */
+        return window;
+    }
+
+    void addScreen(String name, AppScreen screen) {
+        /**
+         * Set this manager as the screen manager and adds it to the list.
+         */
+        screen.setScreensManager(this);
         screens.put(name, screen);
     }
 
     void removeScreen(String name) {
+        /**
+         * Removes one screen from the manager.
+         */
         screens.remove(name);
     }
-    Node getScreen(String name) {
+    AppScreen getScreen(String name) {
+        /**
+         * Gets one screen from the manager.
+         */
         return screens.get(name);
     }
 
     void setActiveScreenTo( String name) {
+        /**
+         * Sets the active screen on the stage. To do that fades out the old one and fades in the new one.
+         */
         if (getChildren().isEmpty()) {
             //setOpacity(0);
-            getChildren().add(screens.get(name));
+            getChildren().add((StackPane) screens.get(name));
             //fadeIn();
         } else {
             fadeOut(actionEvent -> {
                 getChildren().remove(0);
-                getChildren().add(getScreen(name));
+                getChildren().add((StackPane) getScreen(name));
                 fadeIn();
             });
 
@@ -47,19 +80,35 @@ public class ScreensManager extends StackPane {
     }
 
     private void fadeOut(EventHandler<ActionEvent> e) {
-        Timeline timeline = new Timeline(
-                new KeyFrame(Duration.ZERO, new KeyValue(opacityProperty(),1)),
-                new KeyFrame(new Duration(1e3), e, new KeyValue(opacityProperty(), 0))
-        );
-        timeline.play();
+        /**
+         * Fade out effect.
+         */
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), this);
+        fadeOut.setFromValue(1);
+        fadeOut.setToValue(0);
+        fadeOut.setOnFinished(e);
+        fadeOut.play();
+//
+//        Timeline timeline = new Timeline(
+//                new KeyFrame(Duration.ZERO, new KeyValue(opacityProperty(),1)),
+//                new KeyFrame(new Duration(1e3), e, new KeyValue(opacityProperty(), 0))
+//        );
+//        timeline.play();
 
     }
 
     private void fadeIn() {
-        Timeline timeline = new Timeline(
-                new KeyFrame(Duration.ZERO, new KeyValue(opacityProperty(),0)),
-                new KeyFrame(new Duration(1e3), new KeyValue(opacityProperty(), 1))
-        );
-        timeline.play();
+        /**
+         * Fade in effect
+         */
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), this);
+        fadeIn.setFromValue(0);
+        fadeIn.setToValue(1);
+        fadeIn.play();
+//        Timeline timeline = new Timeline(
+//                new KeyFrame(Duration.ZERO, new KeyValue(opacityProperty(),0)),
+//                new KeyFrame(new Duration(1e3), new KeyValue(opacityProperty(), 1))
+//        );
+//        timeline.play();
     }
 }
